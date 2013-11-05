0359dba69e66e586918fd20895d18681ba1bd28c
Dimoncik <Dimitriy012@gmail.com>
2013-11-05 18:38:52 +0300

    Контроллер

---
+++ ChestClient2/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChestClient2.Models;
+
+namespace ChestClient2.Controllers
+{
+    public class UserController : Controller
+
+    {
+        private static readonly Dictionary<string, UserModels> Users = new Dictionary<string, UserModels>();
+        //
+        // GET: /User /
+
+        public ActionResult List()
+        {
+            return Json (Users, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
