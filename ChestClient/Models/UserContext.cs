7466bd340b8d7e355e0a76a1997a8953ffa8e54b
Erv08 <pom13life@gmail.com>
2013-11-09 14:01:08 +0400

    Добавил отправку базы данных в формате json
    1)Изменил название UserModels -> UserModel для более удобочитаемого
    названия во время работы с базами данных;
    2)Добавил UserContext отвечающий за объявление базы данных;
    3)И в UserController прописал возврат базы данных в формате json;

---
+++ ChestClient/Models/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ChestClient.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<UserModel> UserModels { get; set; }
+    }
+}
