e61743b4219706d391a1a45e0e5a168c9d7b989c
1amir1 <dragon.1993@mail.ru>
2013-11-12 16:10:52 +0300

    забытый файл

---
+++ DevExChessConsole/DevExChessConsole/Consts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevExChessConsole
+{
+    public static class Consts
+    { 
+           #if (DEBUG)
+           public  const string domain = "http://localhost:2964/";
+            #else
+          public const string domain = "http://command2.apphb.com/";
+            #endif
+    }
+}
