0a4db74525aa4a6ceeb001938c52507b138894d7
wincess <wincess@mail.ru>
2013-12-17 17:35:37 +0400

    новый тест для ладьи: ладья в центре и вокруг свои

--- UnitTest/AttackMapTest.cs
+++ UnitTest/AttackMapTest.cs
@@ -37,13 +37,50 @@ namespace UnitTest
             for (int j = 1; j <= Board.BoardSize; j++)
             { 
                 if(j!=4)
-                Assert.IsTrue(map["e"+j].Contains(rook));
+                Assert.IsTrue(map["e"+ j].Contains(rook));
 
             }
             for (char i = 'a'; i <= 'h'; i++)
             {
                 if(i!='e')
                 Assert.IsTrue(map[i.ToString() +4].Contains(rook));
+            }
+        }
+
+        /// <summary>
+        /// Одна ладья в центре поля и свои вокруг
+        /// </summary>
+        [TestMethod]
+        public void SimpleRookTest()
+        {
+            //a - arange
+            Board board = new Board();
+            var rook = new FigureRook(Side.WHITE);
+            var knight = new FigureKnight(Side.WHITE);
+            var queen = new FigureQueen(Side.WHITE);
+            var bishop = new FigureBishop(Side.WHITE);
+            var pawn = new FigurePawn(Side.WHITE);
+            board["e4"] = rook;
+            board["e2"] = pawn;
+            board["e6"] = knight;
+            board["f4"] = queen;
+            board["b4"] = bishop;
+
+            //a - act
+            AttackMap map = new AttackMap(new List<Move>(), board);
+            //a - assert
+
+            for (int j = 1; j <= Board.BoardSize; j++)
+            {
+                if (j != 4  && j < 6 && j > 2)
+                    Assert.IsTrue(map["e" + j].Contains(rook));
+                if (j >= 6 && j < 2)
+                    Assert.IsTrue(!map["e" + j].Contains(rook));
+            }
+            for (char i = 'a'; i <= 'h'; i++)
+            {
+                if (i != 'e' && i < 'f' && i > 'b')
+                    Assert.IsTrue(map[i.ToString() + 4].Contains(rook));
             }
         }
     }
