69a405a1debfecc0aabd886c7353246f847eec0f
EgorRudakov <egor-rudakov@mail.ru>
2014-04-02 18:33:26 +0300

    Кнопки *сдаться* и *предложить ничью*.

--- ChestClient/Controllers/GameController.cs
+++ ChestClient/Controllers/GameController.cs
@@ -206,7 +206,7 @@ namespace ChestClient.Controllers
 
         public ActionResult Real(string gameID)
         {
-            return View("Real");
+            return View("Game");
         }
     }
 }
