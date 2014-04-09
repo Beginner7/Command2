a76750d6154decddc9c605d5f0001455f7e99927
EgorRudakov <egor-rudakov@mail.ru>
2014-04-09 18:47:04 +0300

    Оформление чата.

--- ChestClient/Scripts/ChatUI.js
+++ ChestClient/Scripts/ChatUI.js
@@ -15,24 +15,4 @@ $(document).ready(function () {
         .animate({ opacity: "hide" }, "slow");
         return false;
     });
-
-    $(".pane .btn-unapprove").click(function () {
-        $(this).parents(".pane").animate({ backgroundColor: "#fff568" }, "fast")
-        .animate({ backgroundColor: "#ffffff" }, "slow")
-        .addClass("spam");
-        return false;
-    });
-
-    $(".pane .btn-approve").click(function () {
-        $(this).parents(".pane").animate({ backgroundColor: "#dafda5" }, "fast")
-        .animate({ backgroundColor: "#ffffff" }, "slow")
-        .removeClass("spam");
-        return false;
-    });
-
-    $(".pane .btn-spam").click(function () {
-        $(this).parents(".pane").animate({ backgroundColor: "#fbc7c7" }, "fast")
-        .animate({ opacity: "hide" }, "slow");
-        return false;
-    });
 });
