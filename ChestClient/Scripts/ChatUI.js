2aa9e3f0a9064cd316b19d7fb3ac2bc40e689796
EgorRudakov <egor-rudakov@mail.ru>
2014-04-09 17:11:09 +0300

    Перемещение кнопок и чата.

--- ChestClient/Scripts/ChatUI.js
+++ ChestClient/Scripts/ChatUI.js
@@ -4,3 +4,35 @@
         $(this).toggleClass("active");
     });
 });
+$(document).ready(function () {
+
+    $(".pane:even").addClass("alt");
+
+    $(".pane .btn-delete").click(function () {
+        alert("This comment will be deleted!");
+
+        $(this).parents(".pane").animate({ backgroundColor: "#fbc7c7" }, "fast")
+        .animate({ opacity: "hide" }, "slow");
+        return false;
+    });
+
+    $(".pane .btn-unapprove").click(function () {
+        $(this).parents(".pane").animate({ backgroundColor: "#fff568" }, "fast")
+        .animate({ backgroundColor: "#ffffff" }, "slow")
+        .addClass("spam");
+        return false;
+    });
+
+    $(".pane .btn-approve").click(function () {
+        $(this).parents(".pane").animate({ backgroundColor: "#dafda5" }, "fast")
+        .animate({ backgroundColor: "#ffffff" }, "slow")
+        .removeClass("spam");
+        return false;
+    });
+
+    $(".pane .btn-spam").click(function () {
+        $(this).parents(".pane").animate({ backgroundColor: "#fbc7c7" }, "fast")
+        .animate({ opacity: "hide" }, "slow");
+        return false;
+    });
+});
