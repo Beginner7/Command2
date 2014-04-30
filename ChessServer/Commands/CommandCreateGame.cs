d8a29c0df581ad5afd7494a5a419e46bc8fd25dd
JetBrain <andreyzemlevskiy@gmail.com>
2014-04-30 22:03:19 +0300

    фикс

--- ChessServer/Commands/CommandCreateGame.cs
+++ ChessServer/Commands/CommandCreateGame.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using Protocol;
+﻿using Protocol;
 using Protocol.Transport;
 using Newtonsoft.Json;
 
@@ -12,22 +11,15 @@ namespace ChessServer.Commands
         {
             var workRequest = JsonConvert.DeserializeObject<CreateGameRequest>(request);
             var workResponse = new CreateGameResponse();
-            User user;
             if (workRequest.NewPlayer == null)
             {
-                user = Server.CreateRandomNewUser();
-                workRequest.NewPlayer = new User { Name = user.Name };
+                workRequest.NewPlayer = Server.CreateRandomNewUser();
             }
-            else
+            if (!Server.Users.ContainsKey(workRequest.NewPlayer.Name))
             {
-                user = Server.Users.Values.FirstOrDefault(u => u.Name == workRequest.NewPlayer.Name);
+                Server.Users.TryAdd(workRequest.NewPlayer.Name, workRequest.NewPlayer);
             }
-            if (user == null)
-            {
-                workResponse.Status = Statuses.NoUser;
-                return workResponse;
-            }
-            var game = new GameObject(user) {Act = Act.WaitingOpponent};
+            var game = new GameObject(workRequest.NewPlayer) { Act = Act.WaitingOpponent };
 
             if (Server.Games.TryAdd(game.Id, game))
             {
